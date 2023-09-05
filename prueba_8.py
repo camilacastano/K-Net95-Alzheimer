f182b89111238e5564e654c5bf08b32b9ef9a723
Camila Castano <82916777+camilacastano@users.noreply.github.com>
2023-09-05 14:29:42 -0600

    Create prueba_8.py

---
+++ prueba_8.py
@@ -0,0 +1,35 @@
+#PRUEBA 8
+
+model = Sequential()
+model.add(Conv3D(32, kernel_size=(3, 3, 3),
+                        activation='relu',
+                        kernel_initializer='he_uniform', 
+                        input_shape=(X_train.shape[1], 
+                        X_train.shape[2], 
+                        X_train.shape[3],
+                        X_train.shape[4])))
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+model.add(BatchNormalization(center=True, scale=True))
+model.add(Dropout(0.7))
+model.add(Conv3D(16, kernel_size=(3, 3, 3), activation='relu', 
+                        kernel_initializer='he_uniform'))
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+model.add(BatchNormalization(center=True, scale=True))
+model.add(Dropout(0.7))
+model.add(Conv3D(8, kernel_size=(3, 3, 3), activation='relu', 
+                        kernel_initializer='he_uniform'))
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+model.add(BatchNormalization(center=True, scale=True))
+model.add(Dropout(0.5))
+model.add(Conv3D(4, kernel_size=(3, 3, 3), activation='relu', 
+                        kernel_initializer='he_uniform'))
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+model.add(BatchNormalization(center=True, scale=True))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(100, activation='sigmoid',
+                        kernel_initializer='he_uniform',
+                        kernel_regularizer=regularizers.L2(0.01)))
+model.add(Dense(50, activation='relu',
+                        kernel_initializer='he_uniform'))
+model.add(Dense(2, activation='softmax'))
