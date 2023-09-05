d521a58b94f0842a82c93b4f41e3109c7b3dcd20
Camila Castano <82916777+camilacastano@users.noreply.github.com>
2023-09-05 14:21:31 -0600

    Create prueba_4.py

---
+++ prueba_4.py
@@ -0,0 +1,27 @@
+model = Sequential()
+model.add(Conv3D(8, kernel_size=(3, 3, 3),
+                        activation='relu',
+                        kernel_initializer='he_uniform', 
+                        input_shape=(X_train.shape[1], 
+                        X_train.shape[2], 
+                        X_train.shape[3],
+                        X_train.shape[4])))
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+model.add(BatchNormalization(center=True, scale=True))
+model.add(Dropout(0.7))
+model.add(Conv3D(4, kernel_size=(3, 3, 3), activation='relu', 
+                        kernel_initializer='he_uniform'))
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+model.add(BatchNormalization(center=True, scale=True))
+model.add(Dropout(0.7))
+model.add(Conv3D(2, kernel_size=(3, 3, 3), activation='relu', 
+                        kernel_initializer='he_uniform'))
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+model.add(BatchNormalization(center=True, scale=True))
+model.add(Dropout(0.7))
+model.add(Flatten())
+model.add(Dense(30, activation='sigmoid',
+                        kernel_initializer='he_uniform'))
+model.add(Dense(30, activation='relu',
+                        kernel_initializer='he_uniform'))
+model.add(Dense(2, activation='softmax'))
